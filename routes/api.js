e348174ce4d4b320523e1edc36dbc582d7207900
QwesThunder <54608664+QwesThunder@users.noreply.github.com>
2021-06-20 11:18:12 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -223,7 +223,7 @@ router.get('/addapikey', (req, res, next) => {
 
     if (!apikey) return res.json(loghandler.notparam)
     if (!(status && apikeyInput && email && nomorhp && name && age && country && exp)) return res.json(loghandler.notAddApiKey)
-    if (apikey != 'Alphabot') return res.json(loghandler.invalidKey)
+    if (apikey != 'ZasApi') return res.json(loghandler.invalidKey)
 
     try {
         zahirr.insert({
